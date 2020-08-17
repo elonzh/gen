4d46a02a2c4c6e3eff77ae3d0410129b13a16d01
zhouerliang <zhouerliang@yunzhangfang.com>
2020-08-17 21:49:37 +0800

    init dart-io

--- settings.sh
+++ settings.sh
@@ -8,3 +8,5 @@ export HTTP_PROXY="http://172.36.1.97:7890"
 export HTTPS_PROXY="http://172.36.1.97:7890"
 export GH_PROXY="https://elonzh.ghproxy.workers.dev"
 
+# see https://github.com/OpenAPITools/openapi-generator/blob/v4.3.1/docs/generators/dart-dio.md
+# see https://github.com/OpenAPITools/openapi-generator/labels/Client%3A%20Dart
